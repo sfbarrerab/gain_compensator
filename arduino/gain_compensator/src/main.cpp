a33767f5c54e9cb41f8b443db405e5ded8d32c7f
sfbarrerab <sfbarrerab@unal.edu.co>
2023-11-29 01:19:51 +0100

    FreeRTOS implemented with serial communication task and python test script

---
+++ arduino/gain_compensator/src/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>  // add the FreeRTOS functions for Semaphores (or Flags).
+
+
+void task_rxtx_serial(void *pvParameters);
+
+// Declare a mutex Semaphore Handle which we will use to manage the Serial Port.
+SemaphoreHandle_t x_serial_txrx_semaphore;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(10); // This time might be increased if the string is large
+
+  if ( x_serial_txrx_semaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  {
+    x_serial_txrx_semaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore
+    if ( ( x_serial_txrx_semaphore ) != NULL )
+      xSemaphoreGive( ( x_serial_txrx_semaphore ) );  // Make the Serial Port available for use
+  }
+  
+  // Create task
+  xTaskCreate(
+    task_rxtx_serial
+    ,  "Rx Tx serial communication"   // name
+    ,  128  // stack size
+    ,  NULL
+    ,  2  // Priority 
+    ,  NULL );
+  
+  // Task scheduler is automatically initiated
+
+}
+
+void loop() {
+  // task already setup run the whole process
+
+}
+
+void task_rxtx_serial(void *pvParameters){
+  
+  while(true){
+    // take the semaphore, if not available wait for 5ms
+    if ( xSemaphoreTake( x_serial_txrx_semaphore, ( TickType_t ) 5 ) == pdTRUE )
+    {
+      while (!Serial.available());
+      double value = Serial.readString().toDouble();
+      Serial.println(value+1.0);
+      xSemaphoreGive( x_serial_txrx_semaphore );  // give the semaphore
+    }
+    vTaskDelay( 15 / portTICK_PERIOD_MS);
+  }
+  
+}
+
+
