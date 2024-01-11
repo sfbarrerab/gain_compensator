c6c8152b424a1fddc4bcb0bd0cafc2d669e5c6d2
Santiago Barrera [Student] <yx30umaj@lhft.de>
2024-01-11 15:48:33 +0100

    switch test on adafruit library

--- arduino/gain_compensator/src/display_layout.cpp
+++ arduino/gain_compensator/src/display_layout.cpp
@@ -1,10 +1,41 @@
 #include "display_layout.h"
 
-// The FT6206 uses hardware I2C (SCL/SDA)
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// For the one we're using, its 300 ohms across the X plate
 Adafruit_FT6206 ctp = Adafruit_FT6206();
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-int oldcolor, currentcolor;
+boolean RecordOn = false;
+
+void drawFrame()
+{
+    tft.drawRect(FRAME_X, FRAME_Y, FRAME_W, FRAME_H, ILI9341_BLACK);
+}
+
+void redBtn()
+{ 
+    tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_RED);
+    tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_BLUE);
+    drawFrame();
+    tft.setCursor(GREENBUTTON_X + 6 , GREENBUTTON_Y + (GREENBUTTON_H/2));
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setTextSize(2);
+    tft.println("ON");
+    RecordOn = false;
+}
+
+void greenBtn()
+{
+    tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_GREEN);
+    tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_BLUE);
+    drawFrame();
+    tft.setCursor(REDBUTTON_X + 6 , REDBUTTON_Y + (REDBUTTON_H/2));
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setTextSize(2);
+    tft.println("OFF");
+    RecordOn = true;
+}
 
 void init_tft(){
 
@@ -14,21 +45,14 @@ void init_tft(){
         while (1) delay(10);
     }
 
-      
-    tft.fillScreen(ILI9341_BLACK);
-    
-    // make the color selection boxes
-    tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
-    tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
-    tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
-    tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
-    tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
-    tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
-    
-    // select the current color 'red'
-    tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-    currentcolor = ILI9341_RED;
 
+    tft.fillScreen(ILI9341_BLACK);
+    // origin = left,top landscape (USB left upper)
+    tft.setRotation(1); 
+    Serial.println("tft began"); 
+
+    redBtn();
+    Serial.println("red btn created");
 }
 
 
@@ -36,77 +60,48 @@ void task_display(void *pvParameters)
 {
 
     while(1){
-
-      
+             
         // Wait for a touch
         if (ctp.touched()) { 
             // Retrieve a point  
             TS_Point p = ctp.getPoint();
-            
-            /*
-            // Print out raw data from screen touch controller
-            Serial.print("X = "); Serial.print(p.x);
-            Serial.print("\tY = "); Serial.print(p.y);
-            Serial.print(" -> ");
-            */
 
+    
             // flip it around to match the screen.
             p.x = map(p.x, 0, 240, 240, 0);
             p.y = map(p.y, 0, 320, 320, 0);
 
-            // Print out the remapped (rotated) coordinates
-            // if ( xSemaphoreTake( x_serial_txrx_semaphore, SEMAPHORE_BLOCK_TIME ) == pdTRUE ){
+            Serial.print("("); Serial.print(p.x);
+            Serial.print(", "); Serial.print(p.y);
+            Serial.println(")");
 
-            //     Serial.print("("); Serial.print(p.x);
-            //     Serial.print(", "); Serial.print(p.y);
-            //     Serial.println(")");
-            //     xSemaphoreGive( x_serial_txrx_semaphore );
-            // }
 
-            if (p.y < BOXSIZE) {
-                oldcolor = currentcolor;
+            int y = tft.height() - p.x;
+            int x = p.y;
 
-                if (p.x < BOXSIZE) { 
-                    currentcolor = ILI9341_RED; 
-                    tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-                } else if (p.x < BOXSIZE*2) {
-                    currentcolor = ILI9341_YELLOW;
-                    tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-                } else if (p.x < BOXSIZE*3) {
-                    currentcolor = ILI9341_GREEN;
-                    tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-                } else if (p.x < BOXSIZE*4) {
-                    currentcolor = ILI9341_CYAN;
-                    tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-                } else if (p.x < BOXSIZE*5) {
-                    currentcolor = ILI9341_BLUE;
-                    tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-                } else if (p.x <= BOXSIZE*6) {
-                    currentcolor = ILI9341_MAGENTA;
-                    tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-                }
-
-                if (oldcolor != currentcolor) {
-                    if (oldcolor == ILI9341_RED) 
-                        tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
-                    if (oldcolor == ILI9341_YELLOW) 
-                        tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
-                    if (oldcolor == ILI9341_GREEN) 
-                        tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
-                    if (oldcolor == ILI9341_CYAN) 
-                        tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
-                    if (oldcolor == ILI9341_BLUE) 
-                        tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
-                    if (oldcolor == ILI9341_MAGENTA) 
-                        tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
+            if (RecordOn)
+            {
+                if((x > REDBUTTON_X) && (x < (REDBUTTON_X + REDBUTTON_W))) {
+                    if ((y > REDBUTTON_Y) && (y <= (REDBUTTON_Y + REDBUTTON_H))) {
+                    Serial.println("Red btn hit"); 
+                    redBtn();
+                    }
                 }
             }
-            if (((p.y-PENRADIUS) > BOXSIZE) && ((p.y+PENRADIUS) < tft.height())) {
-                tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
-            }
+            else //Record is off (RecordOn == false)
+            {
+                if((x > GREENBUTTON_X) && (x < (GREENBUTTON_X + GREENBUTTON_W))) {
+                    if ((y > GREENBUTTON_Y) && (y <= (GREENBUTTON_Y + GREENBUTTON_H))) {
+                    Serial.println("Green btn hit"); 
+                    greenBtn();
+                    }
+                }
+
+
+            }  
         }
 
-        vTaskDelay(1/portTICK_PERIOD_MS); 
+        vTaskDelay(10/portTICK_PERIOD_MS); 
     }
 }
 
--- arduino/gain_compensator/src/display_layout.h
+++ arduino/gain_compensator/src/display_layout.h
@@ -6,13 +6,40 @@
 #include <Adafruit_FT6206.h>
 #include <Arduino_FreeRTOS.h>
 #include "serial_communication.h"
+#include <TouchScreen.h>
 
-// The display uses hardware SPI, plus #9 & #10
+//Touchscreen X+ X- Y+ Y- pins
+#define YP A3  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 5   // can be a digital pin
+#define XP 4   // can be a digital pin
+
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 150
+#define TS_MINY 120
+#define TS_MAXX 920
+#define TS_MAXY 940
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
 #define TFT_CS 10
 #define TFT_DC 9
-// Size of the color selection boxes and the paintbrush size
-#define BOXSIZE 40
-#define PENRADIUS 3
+
+#define FRAME_X 210
+#define FRAME_Y 180
+#define FRAME_W 100
+#define FRAME_H 50
+
+#define REDBUTTON_X FRAME_X
+#define REDBUTTON_Y FRAME_Y
+#define REDBUTTON_W (FRAME_W/2)
+#define REDBUTTON_H FRAME_H
+
+#define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
+#define GREENBUTTON_Y FRAME_Y
+#define GREENBUTTON_W (FRAME_W/2)
+#define GREENBUTTON_H FRAME_H
 
 void init_tft();
 void task_display(void *pvParameters);
