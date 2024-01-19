eea849970e44c774470780d1b90f085faa44c17a
sfbarrerab <sfbarrerab@unal.edu.co>
2024-01-19 23:45:43 +0100

    typo correction in liscence message

--- arduino/gain_compensator/src/widgets_gui.h
+++ arduino/gain_compensator/src/widgets_gui.h
@@ -2,12 +2,12 @@
 /*
 Library writen by Santiago Barrera
 
-This library is only fro being able to use Adafruit ILI9341 library to create
+The library pourpose is being able to use Adafruit ILI9341 library to create
 some widgets in slow microcontrollers, like arduino uno or arduino mega, where 
-you cannot use LVGL for example.
+you cannot use LVGL, for example.
 
-As it is limited to Adafruit library, some of the widgets are rough created, with
-hot pixel pushed parameters. So, be patient with it.
+As it is limited to Adafruit library, some of the widgets are roughly created, with
+pixel-pushed parameters. So, be patient with it.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -15,7 +15,7 @@ the Software without restriction, including without limitation the rights to
 use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
-The above copyright notice and this permissionotice shall be included in all
+The above copyright notice and this permissio notice shall be included in all
 copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
