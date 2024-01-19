537f5cda3ff2157ddccaa45df0b47f2b184a39d4
sfbarrerab <sfbarrerab@unal.edu.co>
2024-01-19 23:43:16 +0100

    Include copy liscence message

--- arduino/gain_compensator/src/widgets_gui.h
+++ arduino/gain_compensator/src/widgets_gui.h
@@ -1,3 +1,31 @@
+
+/*
+Library writen by Santiago Barrera
+
+This library is only fro being able to use Adafruit ILI9341 library to create
+some widgets in slow microcontrollers, like arduino uno or arduino mega, where 
+you cannot use LVGL for example.
+
+As it is limited to Adafruit library, some of the widgets are rough created, with
+hot pixel pushed parameters. So, be patient with it.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permissionotice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. InO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <SPI.h>       // this is needed for display
 #include <Adafruit_ILI9341.h>
