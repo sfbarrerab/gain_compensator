ad33a2b000d24d7b7295072e989401f86df0918c
sfbarrerab <sfbarrerab@unal.edu.co>
2024-04-12 14:30:43 +0200

    Add two sliders to initial page

--- arduino/gain_compensator/screen_test/src/screen_design.cpp
+++ arduino/gain_compensator/screen_test/src/screen_design.cpp
@@ -24,87 +24,54 @@ void init_layout_screen() {
   slider_t param;
 
   // Slider slot parameters
-  param.slotWidth = 9;           // Note: ends of slot will be rounded and anti-aliased
-  param.slotLength = 200;        // Length includes rounded ends
+  param.slotWidth = SLIDER_WIDTH;           // Note: ends of slot will be rounded and anti-aliased
+  param.slotLength = SLIDER_LENGTH;        // Length includes rounded ends
   param.slotColor = TFT_BLUE;    // Slot colour
   param.slotBgColor = TFT_BLACK; // Slot background colour for anti-aliasing
   param.orientation = H_SLIDER;  // sets it "true" for horizontal
 
   // Slider control knob parameters (smooth rounded rectangle)
-  param.knobWidth = 15;          // Always along x axis
-  param.knobHeight = 25;         // Always along y axis
-  param.knobRadius = 5;          // Corner radius
+  param.knobWidth = SLIDER_KNOB_WIDTH;          // Always along x axis
+  param.knobHeight = SLIDER_KNOB_HEIGHT;         // Always along y axis
+  param.knobRadius = SLIDER_KNOB_RADI;          // Corner radius
   param.knobColor = TFT_WHITE;   // Anti-aliased with slot backgound colour
-  param.knobLineColor = TFT_RED; // Colour of marker line (set to same as knobColor for no line)
+  param.knobLineColor = TFT_BLUE; // Colour of marker line (set to same as knobColor for no line)
 
   // Slider range and movement speed
   param.sliderLT = 0;            // Left side for horizontal, top for vertical slider
-  param.sliderRB = 100;          // Right side for horizontal, bottom for vertical slider
+  param.sliderRB = 130;          // Right side for horizontal, bottom for vertical slider
   param.startPosition = 50;      // Start position for control knob
-  param.sliderDelay = 0;         // Microseconds per pixel movement delay (0 = no delay)
+  param.sliderDelay = SLIDER_DELAY;         // Microseconds per pixel movement delay (0 = no delay)
 
   // Create slider using parameters and plot at 0,0
-  s1.drawSlider(0, 0, param);
-
-  // Show bounding box (1 pixel outside slider working area)
-  int16_t x, y;    // x and y can be negative
-  uint16_t w, h;   // Width and height
-  s1.getBoundingRect(&x, &y, &w, &h);     // Update x,y,w,h with bounding box
-  tft.drawRect(x, y, w, h, TFT_DARKGREY); // Draw rectangle outline
-/*
-  // Alternative discrete fns to create/modify same slider - but fn sequence is important...
-  s1.createSlider(9, 200, TFT_BLUE, TFT_BLACK, H_SLIDER);
-  s1.createKnob(15, 25, 5, TFT_WHITE, TFT_RED);
-  s1.setSliderScale(0, 100);
-  s1.drawSlider(0, 0);
-*/
+ 
+  s1.drawSlider(SLIDER_X0, SLIDER_Y0, param);
+  
   s1.setSliderPosition(50);
+  vTaskDelay(10/ portTICK_PERIOD_MS);
   s1.setSliderPosition(100);
 
   // Update any parameters that are different for slider 2
-  param.slotWidth = 4;
-  param.orientation = V_SLIDER; // sets it "false" for vertical
+  param.sliderLT = 0;     // Top for vertical slider
+  param.sliderRB = 150;       // Bottom for vertical slider
+  param.sliderDelay = SLIDER_DELAY; // 2ms per pixel movement delay (movement is blocking until complete)
 
-  param.knobWidth = 19;
-  param.knobHeight = 19;
-  param.knobRadius = 19/2; // Half w and h so creates a circle
-
-  param.sliderLT = 200;     // Top for vertical slider
-  param.sliderRB = 0;       // Bottom for vertical slider
-  param.sliderDelay = 2000; // 2ms per pixel movement delay (movement is blocking until complete)
-
-  s2.drawSlider(0, 50, param);
-
-  s2.getBoundingRect(&x, &y, &w, &h);
-  tft.drawRect(x, y, w, h, TFT_DARKGREY);
-/*
-  // Alternative discrete fns to create/modify same slider - but fn sequence is important...
-  s2.createSlider(4, 200, TFT_BLUE, TFT_BLACK, V_SLIDER);
-  s2.createKnob(19, 19, 9, TFT_WHITE, TFT_RED);
-  s2.setSliderScale(200, 0, 2000);
-  s2.drawSlider(0, 50);
-*/
-  // Move slider under software control
+  s2.drawSlider(SLIDER_X0, SLIDER_Y0 + DELTA_SLIDER_YPOS, param);
+  
   s2.setSliderPosition(50);
+  vTaskDelay(10/ portTICK_PERIOD_MS);
   s2.setSliderPosition(100);
-
 }
 
 void task_screen_layout(void *pvParameters)
 {
-
   while(1){
-
-
     if (s1.checkTouch(x, y)) {
       Serial.print("Slider 1 = "); Serial.println(s1.getSliderPosition());
     }
     if (s2.checkTouch(x, y)) {
       Serial.print("Slider 2 = "); Serial.println(s2.getSliderPosition());
     }
-
-    
-    
     vTaskDelay(40/ portTICK_PERIOD_MS);
   }
 
--- arduino/gain_compensator/screen_test/src/screen_design.h
+++ arduino/gain_compensator/screen_test/src/screen_design.h
@@ -5,5 +5,17 @@
 #include <TFT_eWidget.h>           // Widget library
 #include "touch_screen.h"
 
+#define SLIDER_WIDTH 12
+#define SLIDER_LENGTH 180
+#define SLIDER_KNOB_RADI 15
+#define SLIDER_KNOB_WIDTH 15
+#define SLIDER_KNOB_HEIGHT 25
+#define SLIDER_DELAY 500
+
+#define SLIDER_X0 50
+#define SLIDER_Y0 50
+
+#define DELTA_SLIDER_YPOS 100
+
 void init_layout_screen();
 void task_screen_layout(void *pvParameters);
